294fbff2ab71f727149b32a4343819882e805687
Mateusz Kościukiewicz <mateusz0958@onet.pl>
2026-03-15 17:18:41 +0100

    wrocono port na default i dodano tabele HealthMetrics

---
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/internal/HealthMetrics.java
@@ -0,0 +1,78 @@
+package pl.wsb.fitnesstracker.healthmetrics.internal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+
+@Entity@Table(name = "health_metrics")
+public class HealthMetrics {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "weight", nullable = false)
+    private Double weight;
+
+    @Column(name = "height", nullable = false)
+    private Double height;
+
+    @Column(name = "heart_rate", nullable = false)
+    private Integer heartRate;
+
+    public HealthMetrics() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Integer getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(Integer heartRate) {
+        this.heartRate = heartRate;
+    }
+}
